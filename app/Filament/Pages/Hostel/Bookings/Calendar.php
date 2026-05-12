a89e3559c44f6d8c6dd4e6e61c29bd9afa9669c0
Mehadi Hasan <mehadi.prof@gmail.com>
2026-05-13 01:47:49 +0600

    fixed calendar menu

--- app/Filament/Pages/Hostel/Bookings/Calendar.php
+++ app/Filament/Pages/Hostel/Bookings/Calendar.php
@@ -13,15 +13,13 @@ class Calendar extends BaseHostelPage
 {
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-calendar';
 
-    protected static string | \UnitEnum | null $navigationGroup = 'Booking & Reservation';
-
     protected static ?string $title = 'Booking Calendar';
 
     protected static ?string $navigationLabel = 'Booking Calendar';
 
     protected static ?string $slug = 'hostel/bookings/calendar';
 
-    protected static ?int $navigationSort = 34;
+    protected static ?int $navigationSort = 2;
 
     protected string $view = 'filament.pages.hostel.bookings.calendar';
 
